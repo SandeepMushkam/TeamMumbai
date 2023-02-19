c9c2a398d4b96e09da4458e0f126e9efc34aa34e
Harish Reddy <Hareshkumarreddy.Peddhagudam1@marist.edu>
2023-02-18 21:06:26 -0500

    db connection details

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "safetravel";
+
+// Create connection
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
